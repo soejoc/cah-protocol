bd6442b8711c2519766c9aeebfdd0439c7850f96
Sören Jochimsen <soeren@jochimsen.email>
2019-03-13 00:27:44 +0100

    added session param

--- src/main/java/io/jochimsen/cahframework/handler/message/MessageHandler.java
+++ src/main/java/io/jochimsen/cahframework/handler/message/MessageHandler.java
@@ -1,8 +1,8 @@
 package io.jochimsen.cahframework.handler.message;
 
 import io.jochimsen.cahframework.protocol.object.message.ProtocolMessage;
-import lombok.RequiredArgsConstructor;
+import io.jochimsen.cahframework.session.Session;
 
-public abstract class MessageHandler<T extends ProtocolMessage> {
-    public abstract void handle(final T protocolMessage);
+public interface MessageHandler<M extends ProtocolMessage, S extends Session> {
+    void handleMessage(final M protocolMessage, final S session);
 }
